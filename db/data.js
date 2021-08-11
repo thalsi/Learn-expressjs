26c4cff350355939d6b9a4d22d5ce0591d914cf0
thalsi <thalseehthalsi609@gmail.com>
2021-08-11 13:38:51 +0530

    CRUD

--- db/data.js
+++ db/data.js
@@ -1,123 +1,63 @@
 let data = [{
     "id": 1,
-    "first_name": "Hube",
-    "last_name": "Yendle",
-    "email": "hyendle0@ycombinator.com",
-    "gender": "Female"
+    "name": "Jerad Doorbar"
 }, {
     "id": 2,
-    "first_name": "Madlen",
-    "last_name": "Wiggett",
-    "email": "mwiggett1@economist.com",
-    "gender": "Female"
+    "name": "Rodolphe Mellody"
 }, {
     "id": 3,
-    "first_name": "Marlee",
-    "last_name": "Gearty",
-    "email": "mgearty2@jalbum.net",
-    "gender": "Male"
+    "name": "Marlee Siggin"
 }, {
     "id": 4,
-    "first_name": "Shermie",
-    "last_name": "Mateo",
-    "email": "smateo3@behance.net",
-    "gender": "Male"
+    "name": "Rachel Kiezler"
 }, {
     "id": 5,
-    "first_name": "Cori",
-    "last_name": "Ewenson",
-    "email": "cewenson4@example.com",
-    "gender": "Female"
+    "name": "Vin Somerbell"
 }, {
     "id": 6,
-    "first_name": "Angelina",
-    "last_name": "Menere",
-    "email": "amenere5@oracle.com",
-    "gender": "Female"
+    "name": "Seward Breede"
 }, {
     "id": 7,
-    "first_name": "Sherilyn",
-    "last_name": "MacDermand",
-    "email": "smacdermand6@example.com",
-    "gender": "Female"
+    "name": "Price Skull"
 }, {
     "id": 8,
-    "first_name": "Hillier",
-    "last_name": "Bemand",
-    "email": "hbemand7@ucoz.ru",
-    "gender": "Male"
+    "name": "Danna Beresfore"
 }, {
     "id": 9,
-    "first_name": "Elly",
-    "last_name": "Trundler",
-    "email": "etrundler8@alibaba.com",
-    "gender": "Male"
+    "name": "Mord Tineman"
 }, {
     "id": 10,
-    "first_name": "Basil",
-    "last_name": "Lathwell",
-    "email": "blathwell9@patch.com",
-    "gender": "Male"
+    "name": "Naoma Geldeford"
 }, {
     "id": 11,
-    "first_name": "Xylina",
-    "last_name": "Irce",
-    "email": "xircea@freewebs.com",
-    "gender": "Male"
+    "name": "Bevvy Pilmer"
 }, {
     "id": 12,
-    "first_name": "Barrett",
-    "last_name": "Haymes",
-    "email": "bhaymesb@cafepress.com",
-    "gender": "Female"
+    "name": "Delmer Saile"
 }, {
     "id": 13,
-    "first_name": "Laurel",
-    "last_name": "Syvret",
-    "email": "lsyvretc@earthlink.net",
-    "gender": "Female"
+    "name": "Gino Randleson"
 }, {
     "id": 14,
-    "first_name": "Dix",
-    "last_name": "Lared",
-    "email": "dlaredd@nature.com",
-    "gender": "Female"
+    "name": "Augustin Wildgoose"
 }, {
     "id": 15,
-    "first_name": "Cam",
-    "last_name": "McLachlan",
-    "email": "cmclachlane@stumbleupon.com",
-    "gender": "Female"
+    "name": "Darn Gledhall"
 }, {
     "id": 16,
-    "first_name": "Ashely",
-    "last_name": "Tacker",
-    "email": "atackerf@narod.ru",
-    "gender": "Male"
+    "name": "Melitta Stubbe"
 }, {
     "id": 17,
-    "first_name": "Marlon",
-    "last_name": "Kibbee",
-    "email": "mkibbeeg@blogspot.com",
-    "gender": "Male"
+    "name": "Pietro Give"
 }, {
     "id": 18,
-    "first_name": "Aldous",
-    "last_name": "Zanardii",
-    "email": "azanardiih@amazonaws.com",
-    "gender": "Male"
+    "name": "Arri De Bischof"
 }, {
     "id": 19,
-    "first_name": "Sonnnie",
-    "last_name": "O'Dooghaine",
-    "email": "sodooghainei@weibo.com",
-    "gender": "Female"
+    "name": "Sib di Rocca"
 }, {
     "id": 20,
-    "first_name": "Sherman",
-    "last_name": "Baylay",
-    "email": "sbaylayj@mail.ru",
-    "gender": "Male"
+    "name": "Tiler Timcke"
 }]
 
 module.exports = data;
--- user/user.js
+++ user/user.js
@@ -8,23 +8,35 @@ router.get('/', (req, res) => {
     res.json({ message: "user list sccefully..!", users: data });
 });
 
-
 router.post('/add', (req, res) => {
-    if (!req.body.first_name || !req.body.last_name || !req.body.gender) {
+    if (!req.body.name) {
         res.status(400);
         res.json({ message: "Bad Request" });
     } else {
         var id = data.length + 1;
         data.push({
             "id": id,
-            "first_name": req.body.first_name,
-            "last_name": req.body.last_name,
-            "email": req.body.email ? req.body.email : '',
-            "gender": req.body.gender
+            "name": req.body.name,
         });
         res.json({ message: "user added successfully..!" });
+        console.log(data);
     }
 });
+
+router.put('/update', (req, res) => {
+    if (!req.body.id) {
+        res.status(400);
+        res.json({ message: "Bad Request" });
+    } else {
+        let length = req.body.id - 1;
+        let editedUser = { id: length, name: req.body.name };
+        let users = data.map(element => element.id !== editedUser.id ? element : editedUser);
+        // data.splice(0, data.length)
+        // data = users;
+        res.json({ message: "Update sccessfully..!" });
+        console.log(users);
+    }
+})
 
 router.delete('/delete', (req, res) => {
     if (!req.body.id) {
@@ -32,6 +44,7 @@ router.delete('/delete', (req, res) => {
         res.json({ message: "Bad Request" });
     } else {
         data.slice(req.body.id - 1, 1);
+        console.log(data);
     }
 })
 
